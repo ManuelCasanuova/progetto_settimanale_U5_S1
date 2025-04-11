153e2163390706f13f4f4700c8cacedc54630c3f
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-04-11 17:04:46 +0200

    Piccoli aggiustamenti

--- src/main/java/gestione/prenotazioni/progetto_settimanale_U5_S1/postazioni/PostazioneRepository.java
+++ src/main/java/gestione/prenotazioni/progetto_settimanale_U5_S1/postazioni/PostazioneRepository.java
@@ -3,7 +3,6 @@ package gestione.prenotazioni.progetto_settimanale_U5_S1.postazioni;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
