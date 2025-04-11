3bda1c47e876ff0ebc3f0a1b920acfd4b446bb0e
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-04-11 09:30:09 +0200

    Initial commit

---
+++ src/main/java/gestione/prenotazioni/progetto_settimanale_U5_S1/ProgettoSettimanaleU5S1Application.java
@@ -0,0 +1,13 @@
+package gestione.prenotazioni.progetto_settimanale_U5_S1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleU5S1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleU5S1Application.class, args);
+	}
+
+}
---
+++ src/test/java/gestione/prenotazioni/progetto_settimanale_U5_S1/ProgettoSettimanaleU5S1ApplicationTests.java
@@ -0,0 +1,13 @@
+package gestione.prenotazioni.progetto_settimanale_U5_S1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleU5S1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
